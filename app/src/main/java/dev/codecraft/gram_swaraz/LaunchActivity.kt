85bd8d450762c222be94eca84f67c41536505fc7
codecraft26 <codescraft26@gmail.com>
2022-04-05 01:45:31 +0530

    changes in launch and making looking good

--- app/src/main/java/dev/codecraft/gram_swaraz/LaunchActivity.kt
+++ app/src/main/java/dev/codecraft/gram_swaraz/LaunchActivity.kt
@@ -1,11 +1,18 @@
 package dev.codecraft.gram_swaraz
 
+import android.content.ClipData.newIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+        Handler().postDelayed({
+            startActivity(Intent(this@LaunchActivity, MainActivity::class.java))
+            finish()
+        },100000)
     }
 }
