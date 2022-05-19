81adc689b4fb15a1b163717951628592ff5d2194
codecraft26 <codescraft26@gmail.com>
2022-05-19 23:57:32 +0530

    adapter for social scheme

--- app/src/main/java/dev/codecraft/gram_swaraz/ui/model/Schemes.kt
+++
@@ -1,6 +0,0 @@
-package dev.codecraft.gram_swaraz.ui.model
-
-data class Schemes (
-    val SchemeName : String="",
-    val SchemeDescription : String=""
-        )
--- app/src/main/java/dev/codecraft/gram_swaraz/ui/schemes/social/SocialSchemeAdapter.kt
+++ app/src/main/java/dev/codecraft/gram_swaraz/ui/schemes/social/SocialSchemeAdapter.kt
@@ -1,36 +1,45 @@
 package dev.codecraft.gram_swaraz.ui.schemes.social
 
-import android.view.LayoutInflater
+import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import dev.codecraft.gram_swaraz.R
-import dev.codecraft.gram_swaraz.ui.model.Schemes
+import dev.codecraft.gram_swaraz.data.model.Scheme
 
-class SocialSchemeAdapter(
-    private val context: SocialSectorFragment,
-    private val dataset: List<Schemes>
-): RecyclerView.Adapter<SocialSchemeAdapter.SocialAdapter>()  {
+class SocialSchemeAdapter(private val SocialList:ArrayList<Scheme>):RecyclerView.Adapter<SocialSchemeAdapter.SocialViewHolder>(){
+    class SocialViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val title:TextView= itemView.findViewById(R.id.scheme_title)
+        
 
-
-
-    class SocialAdapter(private val view: View): RecyclerView.ViewHolder(view){
-        val textView: TextView = view.findViewById(R.id.scheme_title)
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SocialAdapter {
-        val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card_for_fragments, parent, false)
-        return SocialAdapter(adapterLayout)
-    }
-    override fun onBindViewHolder(holder: SocialAdapter, position: Int) {
-        val item = dataset[position]
-        holder.textView.text = item.SchemeName
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SocialViewHolder {
+
+        val itemView=View.inflate(parent.context, R.layout.card_for_fragments, null)
+        return SocialViewHolder(itemView)
 
     }
+
+
+    override fun onBindViewHolder(holder: SocialViewHolder, position: Int) {
+        val social=SocialList[position]
+        holder.title.text=social.title
+        holder.itemView.setOnClickListener{
+            Toast.makeText(,"this is toast message",Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
     override fun getItemCount(): Int {
-        return dataset.size
+       return SocialList.size
     }
-    }
+
+
+}
+
