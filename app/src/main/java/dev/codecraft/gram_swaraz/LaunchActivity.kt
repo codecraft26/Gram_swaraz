779e0d935cc447b2608a238bfd63db627f1dafc2
codecraft26 <codescraft26@gmail.com>
2022-06-01 01:35:38 +0530

    adding organic fragment in ui

--- app/src/main/java/dev/codecraft/gram_swaraz/LaunchActivity.kt
+++ app/src/main/java/dev/codecraft/gram_swaraz/LaunchActivity.kt
@@ -1,17 +1,17 @@
 package dev.codecraft.gram_swaraz
 
-import android.content.ClipData.newIntent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import dev.codecraft.gram_swaraz.ui.login.LoginActivity
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
         Handler().postDelayed({
-            startActivity(Intent(this@LaunchActivity, MainActivity::class.java))
+            startActivity(Intent(this@LaunchActivity, LoginActivity::class.java))
             finish()
         },2000)
     }
--- app/src/main/java/dev/codecraft/gram_swaraz/ui/organicfarming/OrganicFragment.kt
+++ app/src/main/java/dev/codecraft/gram_swaraz/ui/organicfarming/OrganicFragment.kt
@@ -1,18 +1,37 @@
 package dev.codecraft.gram_swaraz.ui.organicfarming
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.codecraft.gram_swaraz.R
+import dev.codecraft.gram_swaraz.data.model.Scheme
 import dev.codecraft.gram_swaraz.databinding.FragmentOrganicBinding
 import dev.codecraft.gram_swaraz.databinding.FragmentProfileBinding
+import dev.codecraft.gram_swaraz.ui.schemes.social.SocialSchemeAdapter
 
 
 class OrganicFragment : Fragment() {
     private lateinit var binding: FragmentOrganicBinding
-    // Inflate the layout for this fragment
+    private lateinit var mAdapter: SocialSchemeAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var socialList:ArrayList<Scheme>
+    private lateinit var db: FirebaseFirestore
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,4 +39,36 @@ class OrganicFragment : Fragment() {
         binding = FragmentOrganicBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView=binding.organicFragmentRecyclerviewer
+        recyclerView.layoutManager= LinearLayoutManager(context)
+        recyclerView.setHasFixedSize(true)
+        socialList=ArrayList()
+        mAdapter= SocialSchemeAdapter(socialList)
+        recyclerView.adapter=mAdapter
+        EvenChangeList()
+    }
+
+    private fun EvenChangeList() {
+        db = FirebaseFirestore.getInstance()
+        db.collection("OrganicFarming").addSnapshotListener { value, error ->
+
+
+            if(error!=null) {
+                Log.e("firebase error", error.message.toString())
+                return@addSnapshotListener
+            }
+            for(dc: DocumentChange in value!!.documentChanges){
+                if(dc.type== DocumentChange.Type.ADDED){
+                    val inquire=dc.document.toObject(Scheme::class.java)
+                    socialList.add(inquire)
+                    mAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+
 }
